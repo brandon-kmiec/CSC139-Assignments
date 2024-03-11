304cfc7a8f9f72c921f891e60f523fbca5edddb0
Brandon Kmiec <114979976+brandon-kmiec@users.noreply.github.com>
2024-03-11 00:14:39 -0700

    Updated wish.c

--- Project 2 - Unix Shell/processes-shell/wish.c
+++ Project 2 - Unix Shell/processes-shell/wish.c
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) {
 
     char *userInputCmd = NULL;
     size_t size = 0;
-    
+
     // run interactive mode if one command line argument
     if(argc == 1) {
         while(true) {
@@ -34,7 +34,7 @@ int main(int argc, char* argv[]) {
             while(args[numArgs] != NULL) {
                 args[++numArgs] = strtok((char *) 0, delimeter);
             } // end while
-            
+
             // continue looping if no command was entered
             // else process the command
             if(numArgs == 0) {
@@ -60,7 +60,7 @@ int main(int argc, char* argv[]) {
             while(args[numArgs] != NULL) {
                 args[++numArgs] = strtok((char *) 0, delimeter);
             } // end while
-            
+
             // continue looping if no command was entered
             // else process the command
             if(numArgs == 0) {
@@ -82,6 +82,8 @@ int main(int argc, char* argv[]) {
 
 
 void processCmd(char* args[], int numArgs, char error_message[]) {
+    char path[] = "/bin";
+
     // exit the shell
     if(strcmp(args[0], "exit") == 0) {
         if(numArgs == 1) {
@@ -92,9 +94,9 @@ void processCmd(char* args[], int numArgs, char error_message[]) {
             exit(0);
         } // end else
     } // end if
-    
+
     // change directory
-    if(strcmp(args[0], "cd") == 0) {
+    else if(strcmp(args[0], "cd") == 0) {
         char *dir;
 
         if(numArgs == 2) {
@@ -106,33 +108,53 @@ void processCmd(char* args[], int numArgs, char error_message[]) {
         } // end else
 
         chdir(dir);
-    } // end if
-    
+    } // end else if
+
+    //path
+    else if(strcmp(args[0], "path") == 0) {
+        if(numArgs == 1) {
+            strcpy(path, "");
+        } // end if
+        else {
+            strcpy(path, "");
+            for(int i = 1; i < numArgs; i++) {
+                strncat(path, args[i], strlen(args[i]));    
+            } // end for
+        } // end else
+    } // end else if
+
     // list directory contents
-    if(strcmp(args[0], "ls") == 0) {
-        char *path = "/bin/ls";
-        
-        int status;
-        pid_t pid;
-        pid = fork();
-        switch(pid) {
-            case -1:
-                fprintf(stderr, error_message, strlen(error_message));
-                exit(0);
-            case 0:
-                execv(path, args);
-                break;
-            default:
-                if(wait(&status) == -1) {
+    else {
+        //if(strcmp(args[0], "ls") == 0) {
+        //char path[] = "/bin/";
+        if(strcmp(args[0], "path") != 0) { 
+            strncat(path, "/", 2);
+            strncat(path, args[0], strlen(args[0]));
+        } // end if        
+            int status;
+            pid_t pid;
+            pid = fork();
+            switch(pid) {
+                case -1:
                     fprintf(stderr, error_message, strlen(error_message));
                     exit(0);
-                } // end if
-                break;
-        } // end switch
+                case 0:
+                    if(execv(path, args) != 0) {
+                        fprintf(stderr, error_message, strlen(error_message));
+                        exit(0);
+                    } // end if
+                    break;
+                default:
+                    if(wait(&status) == -1) {
+                        fprintf(stderr, error_message, strlen(error_message));
+                        exit(0);
+                    } // end if
+                    break;
+            } // end switch
     } // end if
-    
+
 
     //
 
     return;
-} // end processCmd
+    } // end processCmd
